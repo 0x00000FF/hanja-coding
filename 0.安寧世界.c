6ade54e16ee803b32055e8ebe24e653df5579fe6
Kangjun Heo == P.Knowledge <knowledge@patche.me>
2019-07-31 19:25:23 +0900

    Hello, World!

--- 0.安寧世界.c
+++ 0.安寧世界.c
@@ -1,7 +1,23 @@
+// An Example of Hello, World!
+// Rewritten with Hanja
+
 #包含 <標準入出.頭>
 
 整數 進入(整數 因子個, 文子* 因子文[]) 
 {
     書式出力("安寧, 世界!");
+    
+    // 揷入文 can be used instead of puts then:
+    // 揷入文("安寧, 世界!");
+    
     反還 0;
+}
+
+// ---------------------------
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+    printf("Hello, World!");
+    return 0;
 }
